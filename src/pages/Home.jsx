2c38197dfc4c61c5e7b888aabca6096f144525d1
yabtesfu <yabtesfu@gmail.com>
2026-05-27 20:52:09 +0100

    Added Greetings with Name, Greeting, Current Time and Current Date

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import BottomNav from '../components/shared/BottomNav';
+import Greetings from '../components/home/Greetings';
 
 const Home = () => {
   return (
     <section className='bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3'>
-      <div className="flex-[3] bg-red-600"></div>
+      {/* Left Div */}
+      <div className="flex-[3]">
+        <Greetings />
+      </div>
       {/* Right Div */}
       <div className="flex-[2] bg-blue-600"></div>
       <BottomNav />
