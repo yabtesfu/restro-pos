284036307c2721c566a2c7458b9642e0db44e4ef
yabtesfu <yabtesfu@gmail.com>
2026-05-29 16:16:35 +0100

    Added A mini Card that shows the Total Earnings and the in Progress

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import BottomNav from '../components/shared/BottomNav';
 import Greetings from '../components/home/Greetings';
+import { BsCashCoin } from 'react-icons/bs';
+import { GrInProgress } from 'react-icons/gr';
+import MiniCard from '../components/home/MiniCard';
 
 const Home = () => {
   return (
@@ -8,6 +11,10 @@ const Home = () => {
       {/* Left Div */}
       <div className="flex-[3]">
         <Greetings />
+        <div className = "flex items-center w-full gap-3 px-8 mt-8">
+          <MiniCard title="Total Earnings" icon={<BsCashCoin />} number={512} footerNum={1.6} />
+          <MiniCard title="In Progress" icon={<GrInProgress />} number={10} footerNum={2.5} />
+        </div>
       </div>
       {/* Right Div */}
       <div className="flex-[2] bg-blue-600"></div>
